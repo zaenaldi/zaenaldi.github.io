6fa3811e8be1c79cf0535f415df57c9f6c2efdf4
zaenaldi <zainaldizainaldi3@gmail.com>
2024-12-04 21:45:05 +0700

    first commit

---
+++ script/main.js
@@ -0,0 +1,11 @@
+// togle and refponsif
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+navSlide();
